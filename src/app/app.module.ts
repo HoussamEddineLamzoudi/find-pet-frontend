bd5383a1ff4043f2c3b2928a344b5339e20ad28a
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2023-01-16 22:38:27 +0100

    start front end part of find pet app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import
+import {CoreModule} from './core/core.module'
+
+//import component :
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginFormComponent } from './component/forms/login-form/login-form.component';
+import { LoginPageComponent } from './views/auth/login-page/login-page.component';
+import { ButtonComponent } from './component/utils/button/button.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginFormComponent,
+    LoginPageComponent,
+    ButtonComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    colors: {
+      // Configure your color palette here
+      'primary': '#faa500'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
